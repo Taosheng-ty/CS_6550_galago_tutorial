dce6a8dd592ae46e0eadcdf5b48ad88ca63b7d1f
Taosheng-ty <taoyang@cs.utah.edu>
2020-09-15 11:14:30 -0600

    chmod +x galago

--- cmd.sh
+++ cmd.sh
@@ -2,5 +2,6 @@
 wget https://master.dl.sourceforge.net/project/lemur/lemur/galago-3.18/galago-3.18-bin.tar.gz
 tar -xzf ./galago-3.18-bin.tar.gz
 mv galago-3.18-bin galago
+chmod +x galago/bin/galago
 # ./galago/bin/galago build  build.json 
 ./galago/bin/galago build --inputPath=trecText_toy.gz --indexPath=./index/ --filetype=trectext --tokenizer/fields+text
